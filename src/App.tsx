74418050740ac5d25e95a3301e92d39b21a55cf0
akindeleDaniel <ojodanielakindele@gmail.com>
2026-09-03 01:26:02 +0100

    feat: render listing data fetched from backend API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { useState, useEffect } from "react";
 
+type Listing = {
+id: number;
+title: string;
+description: string;
+price: number;
+category: string;
+createdAt: string;// date is a string because when coming from express or json, it comes as a string(stringify)
+};
+
 function App () {
-  const [listings, setListings] = useState([])
+
+
+  const [listings, setListings] = useState<Listing[]>([])
 
   useEffect(() =>{
     fetch("http://localhost:3000/listings")
@@ -9,12 +20,25 @@ function App () {
     .then((data) => setListings(data))
   }, [])
 
-  return(
+  return (
     <div>
       <h1>Vendly</h1>
-      <p>Number of listings: {listings.length}</p>
+      {listings.map((listing) => {
+        return (
+          <div key={listing.id}>
+            <div>{listing.id}</div>
+            <div>{listing.title}</div>
+            <div>{listing.price}</div>
+            <div>{listing.description}</div>
+            <div>{listing.category}</div>
+          </div>
+        )
+
+      })}
     </div>
   )
+
 }
+
 
 export default App
